9aae0709af29e225a9e46c5c6b12f5dec19854ee
BOOPESH-foxy <boopesh.mc@gmail.com>
2024-12-20 01:21:06 +0530

    mod jest for test -2

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,10 +1,14 @@
 const request = require('supertest');
-const app = require('../app');
+const { app, server } = require('../app'); // Import the app and server
 
 describe('GET /', () => {
   it('should return a welcome message', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe('Hello, Node.js CI/CD Pipeline!');
+  });
+
+  afterAll((done) => {
+    server.close(done); // Ensure the server closes after the tests
   });
 });
